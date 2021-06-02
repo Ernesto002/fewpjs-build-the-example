85ac708086ea5f1a4e7368bacfa8a72406dcc019
ernesto <ernesto002@icloud.com>
2021-06-01 20:05:55 -0700

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+const hiddenModal = document.getElementById('modal')
+const body = document.querySelector('body')
+hiddenModal.className = 'hidden'
+hiddenModal
+function modalclassName() {
+  hiddenModal.className = 'hidden'
+}
+function mimicServerError() {
+  mimicServerCall()
+  .then((responce)=>{
+    modalclassName()
+    return responce
+  })
+  .catch(function(error) {
+    hiddenModal.className = ''
+    setTimeout(modalclassName,5000)
+  })
+}
+body.addEventListener('click',function(e){
+  if(e.target.className === 'like-glyph' && modal.className.className === 'hidden'){
+    e.target.className = 'activated-heart'
+    e.target.textContext = FULL_HEART
+  }else if(e.target.className === 'activated-heart' && modal.className === 'hidden'){
+    e.target.className = 'like-glyph'
+    e.target.textContext = EMPTY_HEART
+  }
+})
+mimicServerError()
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
